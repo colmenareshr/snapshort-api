ad2f0171dc20f2a48a0dfd0b0b25b52a118cdb3e
Humberto Colmenares <colmenareshr@gmail.com>
2024-01-13 08:55:32 -0300

    add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { UsersModule } from './users/users.module'
 import { LinksModule } from './links/links.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [UsersModule, LinksModule],
+  imports: [AuthModule, UsersModule, LinksModule],
 })
 export class AppModule {}
