bcba77eb7096247e6c0fd3f5e8aed732f71bd320
Humberto Colmenares <colmenareshr@gmail.com>
2024-01-13 20:39:02 -0300

    signup logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common'
+import { ForbiddenException, Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { AuthDto } from './dto'
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 
 @Injectable()
 export class AuthService {
@@ -10,15 +11,23 @@ export class AuthService {
     //generate hash password
     const hash = await argon.hash(dto.password)
     //save a new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    })
-    delete user.hash
-    //return the saved user
-    return user
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      })
+      delete user.hash
+      //return the saved user
+      return user
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credencials taken')
+        }
+      }
+    }
     return { msg: 'Signing up...' }
   }
 
