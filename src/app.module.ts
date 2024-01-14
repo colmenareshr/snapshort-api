14f77b4fdc3d3f493faba6a793c217c83f3ccfe3
Humberto Colmenares <colmenareshr@gmail.com>
2024-01-14 12:50:03 -0300

    nestjs guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { UsersModule } from './users/users.module'
 import { LinksModule } from './links/links.module'
 import { AuthModule } from './auth/auth.module'
 import { PrismaModule } from './prisma/prisma.module'
+import { UserController } from './users/user.controller';
 
 @Module({
   imports: [
@@ -15,5 +16,6 @@ import { PrismaModule } from './prisma/prisma.module'
     LinksModule,
     PrismaModule,
   ],
+  controllers: [UserController],
 })
 export class AppModule {}
