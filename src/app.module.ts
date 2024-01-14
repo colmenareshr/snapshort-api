9df4cbf7d50088bb409759658543c8ae51b6af83
Humberto Colmenares <colmenareshr@gmail.com>
2024-01-14 08:43:49 -0300

    nestjs config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { UsersModule } from './users/users.module'
 import { LinksModule } from './links/links.module'
 import { AuthModule } from './auth/auth.module'
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [AuthModule, UsersModule, LinksModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UsersModule,
+    LinksModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
